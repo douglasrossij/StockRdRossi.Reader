03ea3684b57d3c156add9f6334c156a20fcfb4b7
Douglas Rossi <douglasrossij@hotmail.com>
2024-07-05 11:41:26 +0200

    [FEAT] Include UserMap for EF

---
+++ src/Application/Repositories/Mapping/UserMap.cs
@@ -0,0 +1,37 @@
+﻿using Domain.Shared.Database.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Application.Repositories.Mapping
+{
+    public class UserMap : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            //Table
+            builder.ToTable("Users");
+
+            //Identity
+            builder.HasKey(u => u.Id);
+            builder.Property(u => u.Id).UseIdentityColumn().ValueGeneratedOnAdd();
+
+            //Properties
+            builder.Property(u => u.UserName)
+                .IsRequired()
+                .HasColumnName("Username")
+                .HasColumnType("VARCHAR")
+                .HasMaxLength(25);
+            
+            builder.Property(u => u.Password)
+                .IsRequired()
+                .HasColumnName("Password")
+                .HasColumnType("VARCHAR")
+                .HasMaxLength(25);
+
+            builder.Property(u => u.IsAdministrator)
+                .IsRequired()
+                .HasColumnName("IsAdministrator")
+                .HasColumnType("BIT");
+        }
+    }
+}
