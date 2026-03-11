6491104c14701c93ff3905cf80ebcb36ee6b3f21
Gabriel Rossi <gabriel.rossi@cuia.tech>
2026-03-11 12:19:44 -0300

    - created the configurations for entity type

---
+++ src/Application/Repositories/Configuration/UserConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using Domain.Shared.Database.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Application.Repositories.Configuration
+{
+    public class UserConfiguration : IEntityTypeConfiguration<User> 
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.ToTable("user");
+
+            builder.HasKey(u => u.Id);
+            builder.Property(u => u.Id)
+                .ValueGeneratedOnAdd();
+
+            builder.Property(u => u.UserName)
+                .HasColumnName("user_name")
+                .IsRequired()
+                .HasColumnType("varchar")
+                .HasMaxLength(100);
+
+            builder.Property(u => u.Password)
+                .HasColumnName("password")
+                .IsRequired()
+                .HasColumnType("varchar")
+                .HasMaxLength (100);
+
+            builder.Property(u => u.IsAdministrator)
+                .HasColumnName("is_administrator")
+                .IsRequired()
+                .HasColumnType("boolean");
+        }
+    }
+}
